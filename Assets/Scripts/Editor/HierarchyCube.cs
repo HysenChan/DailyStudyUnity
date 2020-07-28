946e75caa3a013c5900944d2744ba6a66b15ac28
HysenChan <HysenChan@163.com>
2020-07-28 18:55:29 +0800

    【添加】扩展Unity编辑器代码
    编写测试Unity扩展编辑器代码，包括Project面板、Hierarchy面板和菜单栏-Window面板

---
+++ Assets/Scripts/Editor/HierarchyCube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class HierarchyCube
+{
+    //扩展Hierarchy界面菜单：GameObject/xxx/xx即可
+    [MenuItem("GameObject/My Create/Cube",false,0)]
+    static void CreateCube()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+}
---
+++ Assets/Scripts/Rotate/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private void Update()
+    {
+        //this.transform.Rotate(new Vector3(1, 0, 0));
+        //this.GetComponent<Transform>().Rotate(0, 0, 1);
+        //this.GetComponent<Transform>().Rotate(0, 0, 1, Space.World);
+        //this.transform.Translate(0, 0, 1);
+        //this.transform.Translate(0, 0, 1,Space.Self);
+        this.transform.Translate(0, 0, 1, Space.World);
+    }
+}
