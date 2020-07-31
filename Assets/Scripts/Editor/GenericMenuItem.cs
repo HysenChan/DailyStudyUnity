223503ffe172d82bb3b9be3a99bd692fb1b11495
HysenChan <HysenChan@163.com>
2020-07-31 18:52:21 +0800

    【添加】扩展Unity编辑器代码学习3

---
+++ Assets/Scripts/Editor/GenericMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GenericMenuItem:EditorWindow,IHasCustomMenu
+{
+    void IHasCustomMenu.AddItemsToMenu(GenericMenu menu)
+    {
+        menu.AddDisabledItem(new GUIContent("Disable"));
+        menu.AddItem(new GUIContent("Test1"), true, () => { Debug.Log("Test1"); });
+        menu.AddItem(new GUIContent("Test2"), true, () => { Debug.Log("Test2"); });
+        menu.AddSeparator("Test/");
+        menu.AddItem(new GUIContent("Test/Test3"), true, () => { Debug.Log("Test3"); });
+    }
+
+    [MenuItem("Window/My Window")]
+    static void Init()
+    {
+        GenericMenuItem genericMenu = (GenericMenuItem)GetWindow(typeof(GenericMenuItem));
+        genericMenu.Show();
+    }
+}
---
+++ Assets/Scripts/Editor/PreviewWindows.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//[CustomPreview(typeof(GameObject))]
+//public class PreviewWindows : ObjectPreview
+//{
+//    public override bool HasPreviewGUI()
+//    {
+//        return true;
+//    }
+//    public override void OnPreviewGUI(Rect r, GUIStyle background)
+//    {
+//        GUI.DrawTexture(r, AssetDatabase.LoadAssetAtPath<Texture>("Assets/Texture/Unity.png"));
+//        GUILayout.Label("Hello World!");
+//    }
+//}
+
+public class PreviewObject:EditorWindow
+{
+    private GameObject m_MyGo;
+    private Editor m_Editor;
+
+    [MenuItem("Window/My Windows")]
+    static void Init()
+    {
+        PreviewObject previewObject = (PreviewObject)EditorWindow.GetWindow(typeof(PreviewObject));
+        previewObject.Show();
+    }
+
+    private void OnGUI()
+    {
+        //设置一个游戏对象
+        m_MyGo = (GameObject)EditorGUILayout.ObjectField(m_MyGo, typeof(GameObject), true);
+        if (m_MyGo!=null)
+        {
+            if (m_Editor!=null)
+            {
+                //创建Editor实例
+                m_Editor = Editor.CreateEditor(m_MyGo);
+            }
+            //预览它
+            m_Editor.OnPreviewGUI(GUILayoutUtility.GetRect(500, 500), EditorStyles.whiteLabel);
+        }
+    }
+}
