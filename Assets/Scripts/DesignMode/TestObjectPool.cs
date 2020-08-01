6615da4c9f114330d167a7c5cb1393f835db38d8
HysenChan <HysenChan@163.com>
2020-08-01 10:08:46 +0800

    【添加】【对象池】对象池的使用对比
    通过对比Destroy对象和SetActive对象来计算时间，得出结论：当对象过多的时候，使用对象池模式可以大大降低时间使用率，以及系统性能会表现得更佳

---
+++ Assets/Scripts/DesignMode/TestObjectPool.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class TestObjectPool : MonoBehaviour
+{
+    public GameObject testObject;
+
+    private void Start()
+    {
+        //预热
+        ObjectPool.me.Preload(testObject, 500);
+    }
+
+    //无对象测试
+    public void TestOfNotOP()
+    {
+        Debug.Log("CreateNotOfOP");
+        StartCoroutine(CreateOfNotOP());
+    }
+
+    private IEnumerator CreateOfNotOP()
+    {
+        //统计500帧所用时间
+        float t = 0.0f;
+        //每一帧生成一个对象，定时2s后自动消除
+        for (int i = 0; i < 500; i++)
+        {
+            int x = Random.Range(-30, 30);
+            int y = Random.Range(-30, 30);
+            int z = Random.Range(-30, 30);
+            GameObject newObject = Instantiate(testObject, new Vector3(x, y, z),Quaternion.identity);
+            Destroy(newObject, 2.0f);
+
+            yield return null;
+            t += Time.deltaTime;
+        }
+        Debug.Log("无对象池500帧使用秒数：" + t);
+    }
+
+    //使用对象池测试
+    public void TestOfOP()
+    {
+        Debug.Log("CreateOfOP");
+        StartCoroutine(CreateOfOP());
+    }
+
+    private IEnumerator CreateOfOP()
+    {
+        //统计500帧所用时间
+        float t = 0.0f;
+        //每一帧生成一个对象，定时2s后自动消除
+        for (int i = 0; i < 5000; i++)
+        {
+            int x = Random.Range(-30, 30);
+            int y = Random.Range(-30, 30);
+            int z = Random.Range(-30, 30);
+            GameObject newObject = ObjectPool.me.GetObject(testObject,new Vector3(x,y,z),Quaternion.identity);
+            ObjectPool.me.PutObject(newObject, 2.0f);
+
+            yield return null;
+            t += Time.deltaTime;
+        }
+        Debug.Log("使用对象池500帧使用秒数：" + t);
+    }
+}
