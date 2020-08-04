afff7c6257a2379a94d04bd7b872a728aefee7f0
HysenChan <HysenChan@163.com>
2020-08-04 17:59:54 +0800

    【添加】学习反射

---
+++ Assets/Scripts/EditorExpand/Reflect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+public class Reflect
+{
+    [MenuItem("Tools/CreateConsole")]
+    static void CreateConsole()
+    {
+        Debug.Log("CreateConsole");
+    }
+
+    [MenuItem("Tools/CleanConsole")]
+    static void CleanConsole()
+    {
+        //获取assembly
+        Assembly assembly = Assembly.GetAssembly(typeof(Editor));
+        //反射获取LogEntries对象
+        MethodInfo methodInfo = assembly.GetType("UnityEditor.LogEntries").GetMethod("Clear");
+        //反射调用它的Clear方法
+        methodInfo.Invoke(new object(), null);
+    }
+}
